284925260b5a4511fffa2a802751b6e38f6133a7
Jicheng Hu <jicheng@whu.edu.cn>
2023-07-22 21:00:11 +0800

    add msg

--- converter.js
+++ converter.js
@@ -2,6 +2,7 @@ const inputField = document.getElementById('input-temp');
 const fromUnitField = document.getElementById('input-unit');
 const toUnitField = document.getElementById('output-unit');
 const outputField = document.getElementById('output-temp');
+const outputMsg = document.getElementById('output-msg');
 const form = document.getElementById('converter');
 
 function convertTemp(value, fromUnit, toUnit) {
